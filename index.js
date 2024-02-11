e1c5300cc5d06d1b1641ea99abe1f287ef07fc5d
jaminhoood <princeobj5@gmail.com>
2024-02-11 14:48:44 +0100

    feat: convert from procedual to functional

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const regexStr = /^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/
-
 const testCard = 5325233430109903
 
-console.log(regexStr.test(testCard))
+const validateCard = cardNumber => {
+	const regexStr = /^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/
+	return regexStr.test(cardNumber)
+}
+
+console.log(validateCard(testCard))
